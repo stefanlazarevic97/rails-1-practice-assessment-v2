b06a75a3f71722a2052d144db1b1fe9e17945b94
stefanlazarevic <stefanlaza97@gmail.com>
2023-06-26 21:37:22 -0700

    add-active-record-queries

--- active_record_queries/problems/ar_queries.rb
+++ active_record_queries/problems/ar_queries.rb
@@ -33,19 +33,19 @@
 
 def lowest_score
   # Find the id and title of movies with scores lower than 2
-  
+  Movie.select(:id, :title).where('score < 2')
 end
 
 def wesley_movies
   # Find the id, title, and score of all the movies the actor Wesley Snipes
   # was in.
-
+  Movie.select(:id, :title, :score).joins(:actors).where(actors: {name: 'Wesley Snipes'})
 end
 
 def meg_movies
   # Find the id, title, and year of all the movies Meg Ryan has acted in.
   # Order them in ascending order by year.
-
+  Movie.select(:id, :title, :yr).joins(:actors).where(actors: {name: 'Meg Ryan'}).order(:yr)
 end
 
 def susan_supporting_roles
@@ -54,11 +54,11 @@ def susan_supporting_roles
 
   # Reminder: In the castings table, the lead actor for a movie will
   # have ord = 1 
-
+  Movie.select(:id, :title).joins(:actors).where(actors: {name: 'Susan Sarandon'}).where.not(castings: {ord: 1})
 end
 
 def biggest_cast
   # Find the id and title of the 3 movies with the largest casts (i.e., the most
   # actors)
-
+  Movie.select(:id, :title).joins(:castings).group('movies.id').order('COUNT(castings.id) DESC').limit(3)
 end
